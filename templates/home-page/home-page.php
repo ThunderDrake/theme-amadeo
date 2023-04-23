e8e12ac06028e093bcb9938a940cf85924251678
Timofey Krivenkov <drake9100@mail.ru>
2023-04-24 02:50:36 +0300

    ADD: Секция Gross form

--- templates/home-page/home-page.php
+++ templates/home-page/home-page.php
@@ -12,6 +12,7 @@ ct()->header();
   <?php ct()->template( '/home-page/parts/home-page__popular.php' ); ?>
   <?php ct()->template( '/home-page/parts/home-page__lookbook.php' ); ?>
   <?php ct()->template( '/home-page/parts/home-page__about.php' ); ?>
+  <?php ct()->template( '/home-page/parts/home-page__gross-form.php' ); ?>
 </main>
 <?php
 
---
+++ templates/home-page/parts/home-page__gross-form.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<section class="gross-form">
+  <div class="gross-form__container container">
+    <h2 class="gross-form__title">Для оптовых клиентов</h2>
+    <div class="gross-form__text">У нас действует специальная программа скидок и предложений для оптовых клиентов</div>
+    <form action="#" class="gross-form__form form-gross">
+      <label class="form-gross__label">
+        <input type="text" name="Имя" class="input-reset form-gross__input" placeholder="Ваше имя">
+      </label>
+      <label class="form-gross__label">
+        <input type="text" name="E-mail" class="input-reset form-gross__input" placeholder="Ваш e-mail">
+      </label>
+      <button class="btn-reset form-gross__btn">Узнать больше</button>
+    </form>
+  </div>
+</section>
